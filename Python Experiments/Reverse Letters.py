880e5a3bc69a1b05532863cef2b7732751e156a2
Christian <christianstewart5111@gmail.com>
2023-04-06 08:34:57 +0800

    Added Reverse Letters Dist

--- Python Experiments/Reverse Letters.py
+++ Python Experiments/Reverse Letters.py
@@ -15,18 +15,19 @@ root = tk.Tk()
 root.title("Word Reverser")
 
 input_label = tk.Label(root, text="Enter text:")
-input_label.pack()
+input_label.pack(padx=20, pady=20)
 
-input_text = tk.Text(root, height=10, width=50)
+input_text = tk.Text(root, height=5, width=50)
 input_text.pack()
 
 reverse_button = tk.Button(root, text="Reverse Words", command=reverse_text)
-reverse_button.pack()
+reverse_button.pack(padx=20, pady=20)
 
 output_label = tk.Label(root, text="Reversed text:")
 output_label.pack()
 
-output_text = tk.Text(root, height=10, width=50)
+output_text = tk.Text(root, height=5, width=50)
 output_text.pack()
 
+root.geometry("600x400")
 root.mainloop()
