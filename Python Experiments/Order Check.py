0df91ceac0d3d707f119f84e04a4b29dadf4e237
Christian <christianstewart5111@gmail.com>
2023-05-27 20:00:00 -0600

    Added Files

--- Python Experiments/Order Check.py
+++ Python Experiments/Order Check.py
@@ -1,43 +1,49 @@
 import tkinter as tk
 
-def check_language(input_str):
-    stack = []
-    for char in input_str:
-        if char == 'a':
-            stack.append('a')
-        elif char == 'b':
-            if not stack or stack[-1] != 'a':
-                return 'NO'
-            stack.pop()
-        elif char == 'c':
-            if not stack or stack[-1] != 'b':
-                return 'NO'
-            stack.pop()
-        else:
-            return 'NO'
-    if not stack:
+# Define a function to check whether the given string follows the format a before b and b before c
+def check_string(input_str):
+    a_index = input_str.find('a')
+    b_index = input_str.find('b')
+    c_index = input_str.find('c')
+
+    # Check if the string contains all the required characters in the correct order
+    if a_index != -1 and b_index != -1 and c_index != -1 and a_index < b_index < c_index:
         return 'YES'
     else:
         return 'NO'
 
-def on_submit():
-    input_str = input_entry.get()
-    result_label.config(text=check_language(input_str))
+# Define a function to handle button click event
+def on_click():
+    # Get the input text from the entry widget
+    input_text = entry.get()
 
+    # Call the check_string function to check the input text
+    result = check_string(input_text)
+
+    # Update the result label with the output
+    result_label.config(text=result)
+
+# Create a tkinter window
 root = tk.Tk()
-root.title("Language L Checker")
+root.title('String Checker')
+root.geometry('300x150')
+root.resizable(False, False)
 
-input_label = tk.Label(root, text="Enter input string:")
-input_label.pack(padx=20, pady=20)
+# Create a label for the input text
+label = tk.Label(root, text='Enter a string:')
+label.pack(pady=10)
 
-input_entry = tk.Entry(root)
-input_entry.pack(padx=20, pady=20)
+# Create an entry widget for the input text
+entry = tk.Entry(root, width=30)
+entry.pack()
 
-submit_button = tk.Button(root, text="Submit", command=on_submit)
-submit_button.pack(padx=20, pady=20)
+# Create a button to trigger the checking
+button = tk.Button(root, text='Check', command=on_click)
+button.pack(pady=10)
 
-result_label = tk.Label(root, text="")
-result_label.pack(padx=20, pady=20)
+# Create a label to display the result
+result_label = tk.Label(root, text='')
+result_label.pack()
 
-root.geometry("600x400")
+# Start the tkinter main loop
 root.mainloop()
